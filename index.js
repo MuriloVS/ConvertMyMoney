24035b92a00fecc9accda353bbcbaf0b679fe786
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2021-05-31 21:30:28 -0300

    Inicio

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+// aplicação ouvindo na porta 3000
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
