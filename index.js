c5fef4970b00f3148322deb4669340dc4037f5a9
Murilo Vitória da Silva <40902924+MuriloVS@users.noreply.github.com>
2021-06-02 19:26:10 -0300

    Cotação ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const convert = require('./lib/convert');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,9 +10,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+app.get('/cotacao', (req, res) => {
+  const { cotacao, quantidade } = req.query;
+  const cotacaoFormatada = cotacao.replace(',', '.');
+  const quantidadeFormatada = quantidade.replace(',', '.');
+  const resultado = convert.convert(cotacaoFormatada, quantidadeFormatada);
+
+  if (resultado) {
+    res.render('cotacao', {
+      cotacao: convert.toMoney(cotacaoFormatada),
+      quantidade: convert.toMoney(quantidadeFormatada),
+      resultado: convert.toMoney(resultado),
+    });
+  }
+});
+
 // aplicação ouvindo na porta 3000
 app.listen(3000, (err) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log('Servidor iniciado.');
   }
 });
